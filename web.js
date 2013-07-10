a616d91d09ae8f7b1a277cfbcc07548b219c257d
Michael A'Brial <mjabrial@yahoo.com>
2013-07-10 21:55:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
-var fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
 
-var homepage = fs.readFileSync('index.html');
-
-var buf = new Buffer(homepage);
-
-buf.toString('utf8', 0, buf.length);
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html', 'utf-8');
+  response.send(content);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
